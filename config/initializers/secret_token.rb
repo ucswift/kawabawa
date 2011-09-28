85d267ef10f31196fd14e06aa1b828c820a58e33
Shawn jackson <shawnj@onlineadvisorsinc.com>
2011-09-28 16:00:25 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kawabawa::Application.config.secret_token = 'd299263c9d070c12122a4501c4ee878c57f67143b334509a532c4078ab263408e475562f4a028811814f73985229634e95bca6a4e0489cd2c41833cf1813e3fd'
